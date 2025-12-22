cefe542299c4302ad959cee91d7d21fd4f8895a6
dprashanth-kumar <dprashanthtinku@gmail.com>
2025-12-22 22:49:34 +0530

    first time update main.tf

--- Terraform/EKS/main.tf
+++ Terraform/EKS/main.tf
@@ -1,1 +1,21 @@
+provider "aws" {
+  region = var.region
+}
+module "eks" {
+  source = "terraform-aws-modules/eks/aws"
+  version = "~> 20.0"
+  cluster_name     = var.cluster_name
+  cluster_version  = "1.29"
 
+  vpc_id     = null
+  subnet_ids = []
+
+  eks_managed_node_groups  = {
+    free_tier  = {
+      instance_types  = ["t3.micro"]
+      desired_size    = 1
+      min_size        = 1
+      max_size        = 1
+    }
+  }
+}
